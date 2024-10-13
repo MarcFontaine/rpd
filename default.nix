cfb6d5d450c290632d773ed22de8e94ef5817fbf
MarcFontaine <MarcFontaine@users.noreply.github.com>
2024-10-13 10:42:44 +0200

    nix build:
    * git add flake.nix
    * git add defualt.nix
    * Build lockfile:
      * build the builder : outputs.packages.x86_64-linux.default.lock
      * run the lockfile builder:  bash -c $(nix-build
        /nix/store/llfqds8kcd8xjzg4ciygajjbx957rh6n-refresh.drv --no-link)/bin/refresh ??
      * add lock.json
    
    * build website: nix build
    * serve webpage:
    python3 -m http.server 8888 --bind 127.0.0.1 -d ./result/lib/node_modules/serial-console/dist/
    
    * !!!VISIT page with GOOGLE CHROME
      firefox will just fail

---
+++ default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  config,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-json-v3
+    dream2nix.modules.dream2nix.nodejs-granular-v3
+  ];
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      gnugrep
+      stdenv
+      ;
+  };
+
+#  nodejs-granular-v3 = {
+#    buildScript = ''
+#      tsc ./src/index.ts
+#      mv app.js app.js.tmp
+#      echo "#!${config.deps.nodejs}/bin/node" > index.js
+#      cat index.js.tmp >> index.js
+#      chmod +x ./index.js
+#      patchShebangs .
+#    '';
+#  };
+
+  name = lib.mkForce "serial-console";
+  version = lib.mkForce "0.0.1";
+
+  mkDerivation = {
+    src = lib.cleanSource ./.;
+#    checkPhase = ''
+#      ./app.js | ${config.deps.gnugrep}/bin/grep -q "Hello, World!"
+#    '';
+    doCheck = true;
+  };
+}
