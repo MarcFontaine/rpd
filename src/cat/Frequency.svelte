12e7be1fee7d5232e1d9e84e2e2dcec116d2b11f
MarcFontaine <MarcFontaine@users.noreply.github.com>
2025-03-09 22:42:30 +0100

    refactor frequency dekade code

--- src/cat/Frequency.svelte
+++ src/cat/Frequency.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import DecadeDigit from '../misc/DecadeDigit.svelte';
 import {rig} from '../state.svelte';
+import {setFrequency} from '../cat';
 
 let digits = $derived.by(()=>{
   let d = [];
@@ -13,19 +14,46 @@ let digits = $derived.by(()=>{
 }
 )
 
+
+
+function round(v:number,f:number) {
+  return (Math.round(f/v)*v)
+}
+
+function clamp(f:number) {
+  if (f < 1500000) return 1500000
+    else if (f > 29999999) return 29999999
+    else return f;
+}
+
+function makeOnDelta(v:number) {
+  let accum_scroll = 0;
+  let last_update = 0;
+  const initialFrequency = rig.frequency;
+  return (delta:number) => {
+    accum_scroll += delta * v;
+    const delta_scroll = accum_scroll - last_update;
+    if (Math.abs(delta_scroll) >= v) {
+      const newFreq = round(v, initialFrequency + delta_scroll);
+      last_update += (newFreq - initialFrequency);
+      setFrequency(clamp(newFreq));
+      }
+  }
+}
+
 </script>
 
 <div style="display:flex; flex-direction: row;">
-<DecadeDigit d={digits[7]} v={10000000}/>
-<DecadeDigit d={digits[6]} v={1000000}/>
-<DecadeDigit d={digits[5]} v={100000}/>
-<DecadeDigit d={digits[4]} v={10000}/>
-<DecadeDigit d={digits[3]} v={1000}/>
+<DecadeDigit d={digits[7]} onDelta={makeOnDelta(10000000)}/>
+<DecadeDigit d={digits[6]} onDelta={makeOnDelta(1000000)}/>
+<DecadeDigit d={digits[5]} onDelta={makeOnDelta(100000)}/>
+<DecadeDigit d={digits[4]} onDelta={makeOnDelta(10000)}/>
+<DecadeDigit d={digits[3]} onDelta={makeOnDelta(1000)}/>
 <div style="font-size: 9em;">
 .
 </div>
-<DecadeDigit d={digits[2]} v={100}/>
-<DecadeDigit d={digits[1]} v={10}/>
+<DecadeDigit d={digits[2]} onDelta={makeOnDelta(100)}/>
+<DecadeDigit d={digits[1]} onDelta={makeOnDelta(10)}/>
   
 </div>
 <style>
--- src/misc/DecadeDigit.svelte
+++ src/misc/DecadeDigit.svelte
@@ -2,34 +2,15 @@
 import {rig} from '../state.svelte';
 import {setFrequency} from '../cat';
 
-let { d=0,v } = $props();
-let accum_scroll = 0;
-let last_update = 0;
-const speed = 1/250;
+let { d=0,onDelta } = $props();
 
-function round(f:number) {
-  return (Math.floor(f/v)*v)
-}
-
-function clamp(f:number) {
-  if (f < 1500000) return 1500000
-    else if (f > 29999999) return 29999999
-    else return f;
-}
+const wheelSpeed = 1/250;
+const pointerSpeed = 1/10;
 
 </script>
 <button class={rig.frequencyConfirmed? "frequency_confirmed" : "frequency_not_confirmed"}
-onwheel={(e)=> {
-  accum_scroll -= e.deltaY * speed * v;
-  const delta_scroll = accum_scroll - last_update;
-  const oldFrequency = rig.frequency;
-  if (Math.abs(delta_scroll) >= v) {
-    const newFreq = round(oldFrequency + delta_scroll);
-    last_update += (newFreq - oldFrequency);
-    setFrequency(clamp(newFreq));
-    }
-  }
-}
+onwheel={(e)=> onDelta(- e.deltaY * wheelSpeed)}
+onpointerdown={(e)=> {console.log(e);onDelta(e.movementY * pointerSpeed)}}
 >
 {d}
 </button>
@@ -40,11 +21,13 @@ onwheel={(e)=> {
   font-size: 9em;
   font-weight: bold;
   white-space: pre;
+  touch-action: none;
 }
 .frequency_not_confirmed {
   font-family: "Courier New";
   font-size: 9em;
   font-weight: lighter;
   white-space: pre;
+  touch-action: none;  
 }
 </style>
