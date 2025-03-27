f72e2eceb72df0d93afa266b4349575a25d58996
MarcFontaine <MarcFontaine@users.noreply.github.com>
2025-03-27 21:00:33 +0100

    WIP: USB HID device

--- src/misc/Wheel.svelte
+++ src/misc/Wheel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+let wheel = $state(0);
 let dx = $state(0);
 let dy = $state(0);
 let dz = $state(0);
@@ -6,6 +7,31 @@ let dz = $state(0);
 let accum_x = $state(0);
 let accum_y = $state(0);
 let accum_z = $state(0);
+
+
+const deviceFilter = {
+  filters : [
+    { vendorId: 0x0483, productId: 0x5757, usagePage: 1 }
+  ]
+  };
+
+function handleHidInput(event) {
+  const { data, device, reportId } = event;
+  wheel = data.getInt16(1, true);
+};
+
+async function connectRigControl() {
+  if ("hid" in navigator) {
+//    navigator.hid.addEventListener("connect", ({ device }) => {
+//      console.log(`HID connected: ${device.productName}`);
+//    });
+    const devices = await navigator.hid.requestDevice(deviceFilter);
+    console.log(devices);
+    const rigControl = devices[0];
+    await rigControl.open();
+    rigControl.addEventListener("inputreport",handleHidInput);
+  }
+}
 
 </script>
 
@@ -36,6 +62,18 @@ Accumumated y={accum_y}
 Accumumated z={accum_z}
 </div>
 
+<div class="wheel">
+RigControl USB diagnostic:
+<br>
+wheel={wheel}
+<br>
+
+<button
+  onclick={connectRigControl}
+>
+Connect HID device
+</button>
+</div>
 <style>
 .wheel {
   font-family: "Courier New";
