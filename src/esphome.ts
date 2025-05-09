c9fad52dd63f3c67624339185ae6a6bdea4051c0
MarcFontaine <MarcFontaine@users.noreply.github.com>
2025-05-09 19:39:57 +0200

    Eventsource set withCredentials: true

--- src/esphome.ts
+++ src/esphome.ts
@@ -1,7 +1,7 @@
 import * as State from './state.svelte';
 
 export async function connect (url: string) {
-  const eventSource = new EventSource(url);
+  const eventSource = new EventSource(url,  { withCredentials: true });
   eventSource.addEventListener("state",(event:MessageEvent) => {
     State.espHomeEvent.data = event.data;
   });
