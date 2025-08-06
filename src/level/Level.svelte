811245b26c2dfdb7ff31c60acd15126f14963ef7
MarcFontaine <MarcFontaine@users.noreply.github.com>
2025-08-06 21:20:26 +0200

    WIP: SWR meter calibration

--- src/level/Level.svelte
+++ src/level/Level.svelte
@@ -3,18 +3,29 @@ import PiGauge from './PiGauge.svelte';
 
 import {espHomeEvent} from '../state.svelte';
 
-let forward=$derived(espHomeEvent.powerForward);
-let reverse=$derived(espHomeEvent.powerReverse);
+let forward = $derived(espHomeEvent.powerForward);
+let reverse = $derived(espHomeEvent.powerReverse);
+
+
+const u_min = 0.142;
+const u_max = 2.6;
+const p_max = 150;
+
+function cal (u:number) {
+  const u_normal = (u - u_min) / (u_max - u_min);
+  return Math.round(u_normal * u_normal * p_max);
+}
 
 </script>
 
 <div style="display:flex; flex-direction: row; width: 100%;" >
-  <div style="display:flex; flex-direction: column;" >
-    <PiGauge x={forward} />
-    <span> Power Forward: {forward}</span>
+  <div style="display:flex; flex-direction: column; width:50%;" >
+    <PiGauge x={cal(forward)*100/p_max} />
+    <span> Power Forward: {cal(forward)}</span>
+    <span> raw: {forward}</span>
   </div>
-  <div style="display:flex; flex-direction: column;" >
-    <PiGauge x={reverse} />
-    <span> Power Reverse: {reverse}</span>
+  <div style="display:flex; flex-direction: column; width:50%;" >
+    <PiGauge x={cal(reverse)*100/p_max} />
+    <span> Power Reverse: {cal(reverse)}</span>
   </div>
 </div>
