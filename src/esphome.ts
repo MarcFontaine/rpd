3532202443524a42977c738c471f50aac4818510
MarcFontaine <MarcFontaine@users.noreply.github.com>
2025-08-06 19:12:56 +0200

    Disable authentication for esp web events

--- src/esphome.ts
+++ src/esphome.ts
@@ -1,7 +1,7 @@
 import * as State from './state.svelte';
 
 export async function connect (url: string) {
-  const eventSource = new EventSource(url,  { withCredentials: true });
+  const eventSource = new EventSource(url,  { withCredentials: false });
   eventSource.addEventListener("state",(event:MessageEvent) => {
     State.espHomeEvent.data = event.data;
     const data = JSON.parse(event.data);
