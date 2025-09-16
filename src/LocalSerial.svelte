2635d892a67f0b5a1508e8bb16b40cd60a1feb05
MarcFontaine <MarcFontaine@users.noreply.github.com>
2025-09-16 12:55:01 +0200

    WIP:Untested: toggle RTS and DTR

--- src/LocalSerial.svelte
+++ src/LocalSerial.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
-import { currentProfile} from './state.svelte';
+import { currentProfile } from './state.svelte';
 import * as Config from './config';
 import * as Profile from './profile';
+import { isConnected, setRTS, setDTR } from './serial';
 </script>
 
-<div>
-<button
-  onclick={() => {
-    currentProfile.p = Config.localSerial;
-    Profile.initProfile(Config.localSerial)
-  }}>
-Connect to a Local Serial Port
-</button>
-</div>
+{#if isConnected() }
+  <div>
+  Disconnect
+  <br/>
+  <button onclick={() => setRTS(true)}>
+    Set RTS High
+  </button>
+  <br/>
+  <button onclick={() => setRTS(false)}>
+    Set RTS Low
+  </button>
+  <br/>
+  <button onclick={() => setDTR(true)}>
+    Set DTR High
+  </button>
+  <br/>
+  <button onclick={() => setDTR(false)}>
+    Set DTR Low
+  </button>
+  </div>
+{:else}
+  <div>
+  <button
+    onclick={() => {
+      currentProfile.p = Config.localSerial;
+      Profile.initProfile(Config.localSerial)
+    }}>
+  Connect to a Local Serial Port
+  </button>
+  </div>
+{/if}
