bcdc6e6ee6d4552b0b070cb481f9b348c7bf687b
MarcFontaine <MarcFontaine@users.noreply.github.com>
2026-01-20 19:11:34 +0100

    WIP AntennaTuner control

---
+++ src/cat/Tuner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import {rig, settings} from '../state.svelte';
+import {startAntennaTuning} from '../cat';
+
+{#if settings.showAntennaTuner}
+<div>
+  <button>
+    onclick={startAntennaTuning}
+  >
+    Start Antenna Tuning
+  </button>
+</div>
+{/if}
