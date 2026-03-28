a05e4b496fea150c1f75a3413384946a4421b05e
MarcFontaine <MarcFontaine@users.noreply.github.com>
2026-03-28 17:51:08 +0100

    HamLink: commit missing files

---
+++ src/hamlink/Settings.svelte
@@ -0,0 +1,9 @@
+<script module lang="ts">
+import Option from '../settings/Options.svelte';
+import { enableHamLink } from './hamlink.ts';
+
+</script>
+
+<div>
+  <Option bind:o={enableHamLink.value} d={'Connect to HamLink Browser Extension'} />
+</div>
---
+++ src/hamlink/hamlink.ts
@@ -0,0 +1,40 @@
+import { ConfigVar } from '../config/ConfigVar.svelte';
+
+export const enableHamLink = new ConfigVar(
+    { default: true
+    , path: [ 'cat', 'enableHamLink' ]
+    });
+
+let port = null;
+const EXTENSION_ID = "lkoajfagppjiemkcapkonlmekekncdei"
+
+export function initHamLink() {
+  if (enableHamLink.value) connectToExtension()
+}
+
+function connectToExtension() {
+  if (!port) port = chrome.runtime.connect(EXTENSION_ID);
+
+  port.onMessage.addListener((msg) => {
+    window.dispatchEvent(msgToEvent(msg));
+    console.log("JSON empfangen:", msg);
+  });
+
+  port.onDisconnect.addListener(() => {
+    console.warn("Connection Lost. retrying");
+    port = null;
+    setTimeout(connectToExtension, 1000);
+  });
+}
+
+function msgToEvent(msg) : CustomEvent {
+  switch (msg.type) {
+    case "wsjtx-receive":
+      return new CustomEvent('wsjtx-receive', {
+        detail: msg.payload
+      })
+    default:
+      console.error('Cannot route message', msg);
+    break;
+  }
+}
