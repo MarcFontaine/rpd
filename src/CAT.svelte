a7d2b3dd83538e0f257bf8fb098189a83f479826
MarcFontaine <MarcFontaine@users.noreply.github.com>
2026-05-13 18:32:11 +0200

    Remove unused CAT widgets

--- src/CAT.svelte
+++ src/CAT.svelte
@@ -2,11 +2,8 @@
 import Frequency from './cat/Frequency.svelte';
 import BFO from './cat/BFO.svelte';
 import HID from './hid/HID.svelte';
-import PTT from './cat/PTT.svelte';
-import TXPower from './cat/txpower/TXPower.svelte';
 import Mode from './cat/mode/Mode.svelte';
 import Level from './level/Level.svelte';
-import AntennaTuner from './cat/AntennaTuner.svelte';
 import StatusBar from './status/StatusBar.svelte';
 </script>
 
@@ -14,10 +11,7 @@ import StatusBar from './status/StatusBar.svelte';
   <StatusBar />
   <Frequency />
   <HID />
-  <PTT />
   <Mode />
-  <TXPower />
   <Level />
-  <AntennaTuner />
   <BFO />
 </div>
